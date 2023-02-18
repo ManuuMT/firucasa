1ff5bb9fe78af8b60696e1aa6950047d77813cf6
Emanuel Maldonado <ema14mt@gmail.com>
2023-02-18 18:01:49 -0300

    Morgan added to app

--- server/app.ts
+++ server/app.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
 import usersRoutes from "./router/users.routes";
+import morgan from "morgan";
 
 const app = express();
 
 // middlewares
 app.use(cors());
 app.use(express.json());
+app.use(morgan("dev"));
 
 // routes
 app.use(usersRoutes);
