84a0fb82de01e0e8e800e485b8a4ef189cce5447
Emanuel Maldonado Tuero <emtuero@hotmail.com>
2024-03-17 23:12:33 +0100

    React eslint and prettier configured

---
+++ client/.eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'standard',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'eslint-config-prettier',
+    // 'next/babel',
+    // 'next/core-web-vitals',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'no-unused-vars': 'warn',
+    'comma-dangle': ['error', 'always-multiline'],
+  },
+};
--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,30 +1,30 @@
-import Link from "next/link";
-import BoyWithDog from "../assets/img/BoyWithDog.png";
-import IconDog from "../assets/img/IconDog.png";
+import Link from 'next/link';
+import BoyWithDog from '../assets/img/BoyWithDog.png';
+import IconDog from '../assets/img/IconDog.png';
 
 export default function Home() {
   return (
-    <main className="min-h-screen">
-      <div className="h-full flex justify-center items-center">
-        <div className="flex justify-center w-[50%]">
-          <div className="flex flex-col w-[60%] gap-4 font-semibold">
-            <h1 className="text-5xl">
+    <main className='min-h-screen'>
+      <div className='h-full flex justify-center items-center'>
+        <div className='flex justify-center w-[50%]'>
+          <div className='flex flex-col w-[60%] gap-4 font-semibold'>
+            <h1 className='text-5xl'>
               ¡Encuentra a tu compañero de aventuras!
             </h1>
-            <p className="text-xl">
+            <p className='text-xl'>
               Dale una oportunidad a un perro sin hogar...
             </p>
-            <Link href="/adopciones">
-              <button className="flex items-center gap-2 bg-primary text-white hover:bg-purple-800 transition-all duration-300 w-fit px-5 py-2 rounded-lg">
-                ¡Ver adopciones! <img src={IconDog.src} className="w-7 h-5" />
+            <Link href='/adopciones'>
+              <button className='flex items-center gap-2 bg-primary text-white hover:bg-purple-800 transition-all duration-300 w-fit px-5 py-2 rounded-lg'>
+                ¡Ver adopciones! <img src={IconDog.src} className='w-7 h-5' />
               </button>
             </Link>
           </div>
         </div>
-        <div className="flex flex-col w-[50%]">
-          <div className="w-[400px] h-[600px]">
+        <div className='flex flex-col w-[50%]'>
+          <div className='w-[400px] h-[600px]'>
             <img
-              className="w-full h-full object-contain"
+              className='w-full h-full object-contain'
               src={BoyWithDog.src}
             />
           </div>
