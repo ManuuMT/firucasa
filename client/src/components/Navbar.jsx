9335658e05213c6f6222bf4f8f1e7bcbf4dc0182
Emanuel Maldonado Tuero <emtuero@hotmail.com>
2024-03-18 01:19:34 +0100

    ThemeToggle implemented. Dropdown-menu installed

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,64 +1,56 @@
-"use client";
+'use client';
 
-import Link from "next/link";
-import Logo from "../assets/img/LogoNav.png";
-import { Button } from "@/components/ui/Button";
-import { useTheme } from "next-themes";
+import Link from 'next/link';
+import Logo from '../assets/img/LogoNav.png';
+import { ThemeToggle } from './ThemeToggle';
 
 const items = [
   {
-    href: "#",
-    text: "Nosotros",
+    href: '#',
+    text: 'Nosotros',
     id: 1,
   },
   {
-    href: "#",
-    text: "Adopciones",
+    href: '#',
+    text: 'Adopciones',
     id: 2,
   },
   {
-    href: "#",
-    text: "FAQ",
+    href: '#',
+    text: 'FAQ',
     id: 3,
   },
   {
-    href: "#",
-    text: "Contacto",
+    href: '#',
+    text: 'Contacto',
     id: 4,
   },
 ];
 
 export default function Navbar() {
-  const { setTheme } = useTheme();
-
   return (
-    <header className="w-full px-24 py-8 text-xl font-semibold">
-      <nav className="flex items-center justify-between">
-        <div className="flex items-center">
-          <div className="w-[100] h-16">
-            <Link href="/">
+    <header className='w-full px-24 py-8 text-xl font-semibold'>
+      <nav className='flex items-center justify-between'>
+        <div className='flex items-center'>
+          <div className='w-[100] h-16'>
+            <Link href='/'>
               <img
                 src={Logo.src}
-                alt="Firucasa"
-                className="w-full h-full object-contain"
+                alt='Firucasa'
+                className='w-full h-full object-contain'
               />
             </Link>
           </div>
           <div>
-            {items.map((item) => (
-              <Link key={item.id} href={item.href} className="px-2">
+            {items.map(item => (
+              <Link key={item.id} href={item.href} className='px-2'>
                 {item.text}
               </Link>
             ))}
           </div>
         </div>
         <div>
-          <Button variant="destructive" onClick={() => setTheme("light")}>
-            LIGHT
-          </Button>
-          <Button variant="destructive" onClick={() => setTheme("dark")}>
-            DARK
-          </Button>
+          <ThemeToggle />
         </div>
       </nav>
     </header>
